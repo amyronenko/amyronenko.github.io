c008fc35a98d87e12b7c38f3e32c83822b444abb
amyronenko <36802484+amyronenko@users.noreply.github.com>
2018-04-11 23:09:41 +0300

    Update webpack.config.js

--- DSN/webpack.config.js
+++ DSN/webpack.config.js
@@ -1,18 +1,20 @@
-const path = require('path');
-const webpack = require('webpack');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var path = require('path')
+var webpack = require('webpack')
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
 
 var extractPlugin = new ExtractTextPlugin({
   filename: 'main.css'
-});
+})
 
 module.exports = {
   entry: ['./src/js/app.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js',
-    publicPath: '/dist'
+    // publicPath: '/dist'
   },
   module: {
     rules: [
@@ -32,6 +34,23 @@ module.exports = {
         use: extractPlugin.extract({
           use: ['css-loader', 'sass-loader']
         })
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(jpg|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'img/',
+              publicPath: 'img/'
+            }
+          }
+        ]
       }
     ]
   },
@@ -41,6 +60,10 @@ module.exports = {
       $: 'jquery',
       jQuery: 'jquery'
     }),
-    extractPlugin
+    extractPlugin,
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin(['dist'])
   ]
 }
